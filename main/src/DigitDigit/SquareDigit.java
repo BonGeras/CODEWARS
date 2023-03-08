a6604e625077920a12c0aa109c5dcad649088097
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-03-08 20:50:22 +0100

    new task "Digit*Digit" completed

--- main/src/DigitDigit/SquareDigit.java
+++ main/src/DigitDigit/SquareDigit.java
@@ -2,22 +2,39 @@ package DigitDigit;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringJoiner;
 
 public class SquareDigit {
 
     public int squareDigits(int n) {
-        String numberString = Integer.toString(n);
+        char[] chars = ("" + n).toCharArray();
         List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i < numberString.length() - 1; i++) {
-            int val = Integer.parseInt(String.valueOf(numberString.charAt(i))) * Integer.parseInt(String.valueOf(numberString.charAt(i)));
+        for (char a: chars) {
+            int b = Integer.parseInt(String.valueOf(a));
+            nums.add(b*b);
         }
-        StringBuilder result = new StringBuilder();
-        for (int a : nums) {
-            String val = Integer.toString(a);
-            result.append(val);
+        StringBuilder sb = new StringBuilder();
+        for (int num : nums) {
+            sb.append(num);
         }
-        return Integer.parseInt(String.valueOf(result));
+        return Integer.parseInt(sb.toString());
+    }
+}
+
+/*
+
+SMART APPROACH
+import java.util.stream.Collectors;
+
+public class SquareDigit {
+
+    public int squareDigits(int n) {
+        return Integer.parseInt(String.valueOf(n)
+                                      .chars()
+                                      .map(i -> Integer.parseInt(String.valueOf((char) i)))
+                                      .map(i -> i * i)
+                                      .mapToObj(String::valueOf)
+                                      .collect(Collectors.joining("")));
     }
 
 }
+ */
