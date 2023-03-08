57bbd1e2f369b3f694eb56275894923f158f72b9
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-03-08 22:27:09 +0100

    new task "Create Phone Number" completed

--- main/src/CreatePhoneNumber/Kata.java
+++ main/src/CreatePhoneNumber/Kata.java
@@ -12,3 +12,14 @@ public class Kata {
         return formatted;
     }
 }
+
+
+/*
+
+SMART APPROACH
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+  }
+}
+ */
